1e8ad9216d6c438aab13449b023e7001fceb3f84
AnitaPK <anita.khamitkar@gmail.com>
2026-01-28 10:47:28 +0530

    state

---
+++ ReactJS/ecommerce-frontent/src/pages/LoginPage.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+
+const LoginPage = () => {
+    return (
+        <>
+            <div className="container w-50 mx-auto mt-5">
+                <h3>Login here</h3>
+
+                <form>
+                    <div className="mb-3">
+                        <label for="exampleInputEmail1" className="form-label">Email address</label>
+                        <input type="email" className="form-control" id="exampleInputEmail1" />
+                        <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
+                    </div>
+                    <div className="mb-3">
+                        <label for="exampleInputPassword1" className="form-label">Password</label>
+                        <input type="password" className="form-control" id="exampleInputPassword1" />
+                    </div>
+                    <button type="submit" className="btn btn-primary">Submit</button>
+                </form>
+            </div>
+        </>
+
+    )
+}
+
+export default LoginPage
