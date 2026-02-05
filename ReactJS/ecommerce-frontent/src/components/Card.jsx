5a762fa659fade36271b29eec8190e92ce83391e
AnitaPK <anita.khamitkar@gmail.com>
2026-02-05 11:20:05 +0530

    filter

--- ReactJS/ecommerce-frontent/src/components/Card.jsx
+++ ReactJS/ecommerce-frontent/src/components/Card.jsx
@@ -4,11 +4,12 @@ import {Link} from 'react-router-dom'
 const Card = ({prod}) => {
   return (
     <div className="col-12 col-md-6 col-lg-3">
-                            <div className="card" style={{width: "18rem"}}>
+                            <div className="card" style={{width: "14.7rem"}}>
                                 <img src={prod.thumbnail} className="card-img-top" alt="..." />
                                     <div className="card-body">
                                         <h5 className="card-title">{prod.title}</h5>
                                         <p className="card-text text-truncate">{prod.description}</p>
+                                        <p>Price: {prod.price}</p>
                                         <Link to={`/${prod.id}`} className="btn btn-primary">Get More Info</Link>
                                     </div>
                             </div>
--- ReactJS/ecommerce-frontent/src/pages/Dashboard.jsx
+++ ReactJS/ecommerce-frontent/src/pages/Dashboard.jsx
@@ -1,11 +1,16 @@
 import axios from 'axios'
 import React, { useEffect, useState } from 'react'
 import Card from '../components/Card'
+import { FaFilter } from "react-icons/fa";
 
 const Dashboard = () => {
     const [products, setProducts] = useState([])
-    const [searchKey,setSearchKey] = useState("")
-    const [filterProducts,setFilterProducts] = useState([])
+    const [searchKey, setSearchKey] = useState("")
+    const [filterProducts, setFilterProducts] = useState([])
+    const [minPrice, setMinPrice] = useState()
+    const [maxPrice, setMaxPrice] = useState()
+    const [brands, setBrands] = useState([])
+    const [brandInput, setBrandInput] = useState()
 
     // fetchData from api 
     async function fetchData() {
@@ -19,52 +24,109 @@ const Dashboard = () => {
     useEffect(() => {
         fetchData()
     }, [])
+    // search products 
+    // const handleSearchKey =(e)=>{
+    //     e.preventDefault()
+    //     const value1 = (e.target.value)
+    //     console.log(value1)
+    //     setSearchKey(value1)
+    // console.log(searchKey,"searchKey")
+    //     const filterProd = products.filter((p)=>{
+    //         console.log(p.id, searchKey)
+    //         return p.title.toLowerCase().includes(searchKey.toLowerCase())
+    // })
+    //     setFilterProducts(filterProd)
+    // }
 
-// search products 
-// const handleSearchKey =(e)=>{
-//     e.preventDefault()
-//     const value1 = (e.target.value)
-//     console.log(value1)
-//     setSearchKey(value1)
-// console.log(searchKey,"searchKey")
-//     const filterProd = products.filter((p)=>{
-//         console.log(p.id, searchKey)
-//         return p.title.toLowerCase().includes(searchKey.toLowerCase())
-// })
-//     setFilterProducts(filterProd)
-// }
+
+    useEffect(() => {
+        const filterProd = products.filter((p) => {
+            console.log(p.id, searchKey)
+            return p.title.toLowerCase().includes(searchKey.toLowerCase())
+        })
+        setFilterProducts(filterProd)
+
+    }, [searchKey])
+
+    function handleFilter() {
+        console.log("234567890")
+        const filterByPrice = products.filter((p) => {
+            return p.price >= minPrice && p.price <= maxPrice
+        })
+        setFilterProducts(filterByPrice)
+    }
 
 
-useEffect(()=>{
-            const filterProd = products.filter((p)=>{
-        console.log(p.id, searchKey)
-        return p.title.toLowerCase().includes(searchKey.toLowerCase())
-})
-    setFilterProducts(filterProd)
-},[searchKey])
+
+    function getBrandNames() {
+        const b = products.map((p) => p.brand)
+        const uniqueBrands = [...new Set(b)];
+        setBrands(uniqueBrands);
+    }
+
+    useEffect(() => {
+        getBrandNames()
+    }, [products])
+
+    console.log(brands)
+function handleFilterByBrand(){
+    const filterByBrand = products.filter((p)=>{
+        return p.barnd == brandInput
+    })
+    setFilterProducts(filterByBrand)
+}
 
     return (
         <>
             <nav>
                 <div className='ms-5'>
-                    <input 
-                    type="search" 
-                    // onChange={handleSearchKey}
-                    onChange={(e)=>setSearchKey(e.target.value)}
-                    placeholder='search'
+                    <input
+                        type="search"
+                        // onChange={handleSearchKey}
+                        onChange={(e) => setSearchKey(e.target.value)}
+                        placeholder='search'
                     // value={searchKey}
-
                     />
                     <span className='badge rounded-pill text-bg-primary'>{filterProducts.length}</span>
                 </div>
+
             </nav>
             <div className="container">
                 <div className="row">
-                    {filterProducts.map((prod, index) => (
-                        <Card prod={prod} key={prod.id}/>
-                    ))}
+                    <div className="col-md-2 bg-secondary">
+                        Min price:
+                        <input type="range" min={0} max={500}
+                            onChange={(e) => setMinPrice(e.target.value)} />
+                        Max price:
+                        <input type='range' min={0} max={500}
+                            onChange={(e) => setMaxPrice(e.target.value)}
+                        />
+                        <button onClick={() => handleFilter()}
+                            className='btn btn-primary'
+                        ><FaFilter />Filter</button>
+                        <hr />
+                        <select class="form-select"
+                        onChange={(e)=>setBrandInput(e.target.value)}
+                        >
+                            <option selected>Select Brand</option>
+                            {brands?.map((bName)=>(
+                            <option value={bName}>{bName}</option>
+                            ))}
+                        </select>
+                        <button onClick={()=>handleFilterByBrand()}>Filter By Brand</button>
+                    </div>
+                    <div className="col-md-10">
+                        <div className="container">
+                            <div className="row">
+                                {filterProducts.map((prod, index) => (
+                                    <Card prod={prod} key={prod.id} />
+                                ))}
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
+
         </>
     )
 }
